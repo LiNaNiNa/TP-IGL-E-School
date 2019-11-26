9368c00389ad7959af24cbc4b80bbbc597bcdf59
BadreddineZatout <hb_zatout@esi.dz>
2019-11-26 12:45:30 +0100

    modified

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateEnseignementsTable extends Migration
+class CreatePasswordResetsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class CreateEnseignementsTable extends Migration
      */
     public function up()
     {
-        Schema::create('enseignements', function (Blueprint $table) {
-            $table->Integer('Enseignant_id')->primary();
-            $table->String('Code_Mat',10);
-            $table->String('Sect',1);
-            $table->integer('Gr');
+        Schema::create('password_resets', function (Blueprint $table) {
+            $table->string('email')->index();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
         });
     }
 
@@ -28,6 +27,6 @@ class CreateEnseignementsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('enseignements');
+        Schema::dropIfExists('password_resets');
     }
 }
